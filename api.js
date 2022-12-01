f702d256caea176ff3622a6db15da5c8c0ffe6a9
GustavoB017 <gustavoa.017@gmail.com>
2022-11-30 21:14:34 -0500

    Variables de entorno

--- api.js
+++ api.js
@@ -2,7 +2,26 @@ var mongoose = require("mongoose");
 var express = require("express");
 var TaskModel = require('./task_schema');
 var router = express.Router();
-var query = "mongodb+srv://gus_parquesoft:1234@cluster0.vroxppg.mongodb.net/taskBD?retryWrites=true&w=majority"
+
+let environment = null;
+
+if (!process.env.ON_HEROKU) {
+    console.log("Cargando variables de entorno desde archivo");
+    const env = require('node-env-file');
+    env(__dirname + '/.env');
+}
+
+environment = {
+    DBMONGOUSER: process.env.DBMONGOUSER,
+    DBMONGOPASS: process.env.DBMONGOPASS,
+    DBMONGOSERV: process.env.DBMONGOSERV,
+    DBMONGO: process.env.DBMONGO,
+};
+
+var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
+
+
+//var query = "mongodb+srv://gus_parquesoft:1234@cluster0.vroxppg.mongodb.net/taskBD?retryWrites=true&w=majority"
 
 const db = (query);
 
